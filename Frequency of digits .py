26905603311c55183b4c93e9884bf37adf6e0314
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-03-26 10:22:12 +0530

    Create Frequency of digits .py

---
+++ Frequency of digits .py
@@ -0,0 +1,31 @@
+#question
+Write a program to compute the frequency of digits in a given number
+
+Input Format
+
+Input contains integer
+
+Constraints
+
+1<=n<=100000
+
+Output Format
+
+Print the frequency of each digit
+
+Sample Input 0
+
+1122
+
+Sample Output 0
+
+1:2
+2:2
+
+*********************************************************************************
+#answer
+a=list(input())
+b=list(set(a))
+b.sort()
+for i in b:
+print(i+':'+str(a.count(i)))
