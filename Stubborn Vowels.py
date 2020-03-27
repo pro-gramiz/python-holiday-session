5eb67ef86f9a713c8b2e25f7f2d03426e0ba8112
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-03-27 18:11:10 +0530

    Create  Stubborn Vowels.py

---
+++ Stubborn Vowels.py
@@ -0,0 +1,28 @@
+#question
+In english alphabets there are two types of words, vowels and consonents.You are writing a program to reverse a given string, but the vowels are stubborn to move away from their position. So given a string where the vowels are stubborn, print what will be word if the entire word is reversed except for the vowels.
+
+Input Format
+
+One string input
+
+Constraints
+
+3<=String length<=10^5
+
+Output Format
+
+Rversed string output
+
+Sample Input 0
+
+missing
+
+Sample Output 0
+
+ngissim
+Explanation 0
+
+m + i + ss + i + ng = ng + i + ss + n The string are taken as fragments {m,ss,ng} and the set is reversed and placed between the vowels.
+
+****************************************************************************
+#answer
