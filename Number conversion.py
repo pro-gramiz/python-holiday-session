d90aa490ff0e744c9cf4defc430c7101bab34f4b
pooranya <52210652+pooranya@users.noreply.github.com>
2020-03-28 12:32:46 +0530

    Create Number conversion.py

---
+++ Number conversion.py
@@ -0,0 +1,29 @@
+#ans
+a=int(input())
+b=bin(a)
+c=str(b)
+print(c[2::])
+
+
+
+#que
+Write a program to convert decimal number into binary number
+
+Input Format
+
+Input contains integer
+
+Constraints
+
+-2147483647 ≤ num ≤ 2147483647
+
+Output Format
+
+print the values in binary format
+
+Sample Input 0
+
+12
+Sample Output 0
+
+1100
