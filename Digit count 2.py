2eae08044b5c3b6bc56a5539f4c95d1259083045
pooranya <52210652+pooranya@users.noreply.github.com>
2020-03-30 18:43:07 +0530

    Create Digit count 2.py

---
+++ Digit count 2.py
@@ -0,0 +1,32 @@
+#ans
+no=int(input())
+count=0
+while(no>0):
+     no=no//10
+     count+=1
+print(count)
+
+
+
+
+#que
+Given a Number N, find the number of digits in the given number
+
+Input Format
+
+A integer input
+
+Constraints
+
+1<=N<=10^50
+
+Output Format
+
+One integer output
+
+Sample Input 0
+
+19606524803580981401080963834091988718121158294792
+Sample Output 0
+
+50
