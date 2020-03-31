85cbb5bd4f952c51fe3b42146c112b4a09c0721a
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-03-31 17:42:23 +0530

    Create Convert Base.py

---
+++ Convert Base.py
@@ -0,0 +1,28 @@
+#question
+Given a number of with base A to base B, where A is the smallest number which has two fsactors, and B is a number which a number which is formed by multiplyig the unique terms in the prime factorised form C, where C is the tenth number which has odd number of factor.
+
+Input Format
+
+One integer input N
+
+Constraints
+
+1<=N<=10^18
+
+Output Format
+
+Base converted output
+
+Sample Input 0
+
+11101000010001010
+
+Sample Output 0
+
+118922
+
+
+*********************************************************************
+#answer
+n=input()
+print(int(n,2))
